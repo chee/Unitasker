2cd7f38e4e42450130e0d21b8b9fe3f0713c4fc8
chee <chee@snake.dog>
2018-10-17 23:35:52 +0100

    Initial Commit

---
+++ Unitasker/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Unitasker
+//
+//  Created by chee on 17/10/2018.
+//  Copyright © 2018 snoots & co. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+	func applicationDidFinishLaunching(_ aNotification: Notification) {
+		// Insert code here to initialize your application
+	}
+
+	func applicationWillTerminate(_ aNotification: Notification) {
+		// Insert code here to tear down your application
+	}
+
+
+}
+
---
+++ Unitasker/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Unitasker
+//
+//  Created by chee on 17/10/2018.
+//  Copyright © 2018 snoots & co. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+
+		// Do any additional setup after loading the view.
+	}
+
+	override var representedObject: Any? {
+		didSet {
+		// Update the view, if already loaded.
+		}
+	}
+
+
+}
+
